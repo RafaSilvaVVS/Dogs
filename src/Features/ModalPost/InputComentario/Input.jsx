d752ac235dca4893490d45f8db159aaf6d06e72b
RafaSilvaVVS <RafaSilvaVVS@users.noreply.github.com>
2025-07-31 20:53:03 -0300

    Modal da postagens concluida

---
+++ src/Features/ModalPost/InputComentario/Input.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Input.css';
+
+const Input = () => {
+  return (
+    <>
+      <input
+        className="input-comentario"
+        type="comentario"
+        id="comentario"
+        name="comentario"
+      />
+    </>
+  );
+};
+
+export default Input;
